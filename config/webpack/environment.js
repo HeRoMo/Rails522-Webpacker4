390f2a8afd39fcba9728a1cc3dbe03cf1dcbd0ad
HeRoMo <hero@asterisk-works.jp>
2018-12-08 01:29:23 +0900

    add splitChunks config

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -10,4 +10,27 @@ environment.loaders.append('vue', vue)
 environment.loaders.append('pug', pug)
 environment.loaders.append('sass', sass)
 environment.loaders.append('typescript', typescript)
+
+const WebpackAssetsManifest = require('webpack-assets-manifest');
+
+const splitChunks = {
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    },
+  },
+};
+
+environment.config.merge(splitChunks);
+
+// Should override the existing manifest plugin
+environment.plugins.insert(
+  'Manifest',
+  new WebpackAssetsManifest({
+    entrypoints: true, // default in rails is false
+    writeToDisk: true, // rails defaults copied from webpacker
+    publicPath: true // rails defaults copied from webpacker
+  })
+)
+
 module.exports = environment
