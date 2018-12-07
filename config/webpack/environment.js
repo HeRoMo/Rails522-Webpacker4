0915cde5420f436b8e3cfddb8f1728cdcac1bd1e
HeRoMo <hero@asterisk-works.jp>
2018-12-08 00:40:20 +0900

    add pug support

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,8 +2,10 @@ const { environment } = require('@rails/webpacker')
 const typescript =  require('./loaders/typescript')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue =  require('./loaders/vue')
+const pug =  require('./loaders/pug')
 
 environment.plugins.append('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.append('vue', vue)
+environment.loaders.append('pug', pug)
 environment.loaders.append('typescript', typescript)
 module.exports = environment
